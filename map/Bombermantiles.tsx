86bc3b6c3516d61179b9d92cd73df6f6f655bcc9
JoaoSnd <joaopedrosnd@gmail.com>
2023-11-21 17:53:03 -0300

    mapa

---
+++ map/Bombermantiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Bomberman" tilewidth="16" tileheight="16" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="grama.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="parede-destruivel.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="parede-destruivel-marrom.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="parede-fixa.png"/>
+ </tile>
+</tileset>
